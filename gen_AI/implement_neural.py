390ace3e738a4667245d948aafa3750eaf0c5c67
dipak1920basnet <dipak1920basnet@gmail.com>
2025-12-03 20:17:54 +0545

    create and load the dataset

--- gen_AI/implement_neural.py
+++ gen_AI/implement_neural.py
@@ -1,2 +1,22 @@
 # Implentation of neural network in tensorflow
 
+import pandas as pd 
+import numpy as np 
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+
+
+# Create and load the dataset
+data = {
+    'feature1': [0.1, 0.2, 0.3, 0.4, 0.5],
+    'feature2': [0.5, 0.4, 0.3, 0.2, 0.1],
+    'label': [0, 0, 1, 1, 1]
+}
+
+df = pd.DataFrame(data)
+X = df[['feature1', 'feature2']]
+y = df[['label']]
+
+
+# 
